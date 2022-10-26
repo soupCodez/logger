ac9180721123fae64b58b0e67d13f11ac5f79c36
SouSeeGold <bhadsour12@gmail.com>
2022-10-26 19:01:10 +0000

    init commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const chalkPipe = require('chalk-pipe');
+
+const error = (log) => {
+    console.log(chalkPipe('red.bold')(`ERROR: ${log}`))
+}
+
+module.exports.logger.error = error
+
+const warn = (log) => {
+    console.log(chalkPipe('yellow.italic.bold')(`WARN: ${log}`))
+}
+
+module.exports.logger.warn = warn
+
+const success = (log) => {
+    console.log(chalkPipe('green.bold')(`SUCCESS: ${log} ✅`))
+}
+
+module.exports.logger.success = success
+
+const info = (log) => {
+    console.log(chalkPipe('white.bold')(`INFO: ${log}`))
+}
+
+module.exports.logger.info = info
---
+++ test.js
@@ -0,0 +1,3 @@
+const logger = require('./index.js')
+
+logger.info('lol')
